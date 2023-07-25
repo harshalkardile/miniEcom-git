932b402f3f15f843b4cbab12a69a2a1c0fc27d4a
harshal <kardileharshal567@gmail.com>
2023-07-25 10:02:57 +0530

    comments added to the js file

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const submitted = e => {
     
+    //To assign the data came from the textfields to the variables 
     var name = document.getElementById('name').value;
     var price = document.getElementById('number').value;
     var img = document.getElementById('img').value;
     // alert(name);
 
+    //To store the data in the local storage
     const Product=JSON.parse(localStorage.getItem('Product')) || [];
     Product.push({
         name: name,
@@ -12,6 +14,7 @@ const submitted = e => {
         image: img
     })
 
+    
     const jsonObject=JSON.stringify(Product);
     localStorage.setItem('Product', jsonObject);
     console.log(localStorage.getItem('Product'));
@@ -23,7 +26,11 @@ const submitted = e => {
     //  var img= localStorage.setItem("img", img);
 
     }
+
+    //To get the id from the div
     const output = document.querySelector("#card");
+
+    //Asyncronus display funtion to render the data on webpage
     const dispData= async () => {
         console.log(localStorage.getItem('Product'));
         if(localStorage.getItem('Product')){
@@ -32,19 +39,22 @@ const submitted = e => {
             let dataDisplay = JSON.parse(localStorage.getItem('Product')).map(data => {
             const{ name, price, image } = data;
             return `
-                
                     <img src="images/./$data.image">
                     <h4>data.name</h4>
-                    <p>$data.price</p>
-            
-                `
+                    <p>data.price</p>
+                    `
             }).join("");
             output.innerHTML=dataDisplay;     
         }
     }
 
-    // {$data.image}{$data.price}{$data.name}
+
+    // calling declared function
     dispData();
+
+
+    // {$data.image}{$data.price}{$data.name}
+    
  // output.innerHTML += `
             //     <div class="card"><img src="images/{$data.image}"><h4>{$data.name}</h4>
             //     <p>{$data.price}</p></div>
